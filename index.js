71f5c9d697daa0adf96363bd3a5d553f21d8ab61
Hayden Roney <roneyhayden@gmail.com>
2023-10-18 12:19:40 -0500

    Lab finished

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com",
+        })
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            const body = document.querySelector("body");
+            body.innerHTML = `<p>${data.id}</p>`;
+        })
+        .catch((error) => {
+            const body = document.querySelector("body");
+            body.innerHTML = `<p>${error}</p>`;
+        })
+};
